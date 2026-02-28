23dbc5d30a32193008ecaabd0c0d7aae937f9885
NextTechOKE <Jacewilliams7144@gmail.com>
2026-02-27 19:54:48 -0500

    Added climber logic

---
+++ src/main/java/frc/robot/commands/ClimberDown.java
@@ -0,0 +1,29 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.climber.Climber;
+
+public class ClimberDown extends Command {
+
+  private final Climber climber;
+
+  public ClimberDown(Climber climber) {
+    this.climber = climber;
+    addRequirements(climber);
+  }
+
+  @Override
+  public void initialize() {
+    climber.climberUp();
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    climber.stop();
+  }
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/climber/Climber.java
@@ -0,0 +1,61 @@
+package frc.robot.subsystems.climber;
+
+import static edu.wpi.first.units.Units.*;
+
+import com.ctre.phoenix6.configs.*;
+import com.ctre.phoenix6.controls.*;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.*;
+import edu.wpi.first.units.measure.*;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Climber extends SubsystemBase {
+
+ 
+    private static final double CLIMBER_UP_SPEED = 0.2;
+    private static final double CLIMBER_DOWN_SPEED = -0.2;
+
+
+  private final TalonFX climberMotor;
+
+  private final VoltageOut rollerRequest = new VoltageOut(0);
+  private final MotionMagicVoltage pivotRequest = new MotionMagicVoltage(0);
+
+  // CAN IDs
+  public Climber(int climberID) {
+   climberMotor = new TalonFX(climberID);
+
+    configureClimber();
+
+  }
+
+  private void configureClimber() {
+    TalonFXConfiguration config =
+        new TalonFXConfiguration()
+            .withMotorOutput(new MotorOutputConfigs().withNeutralMode(NeutralModeValue.Brake))
+            .withCurrentLimits(
+                new CurrentLimitsConfigs()
+                    .withStatorCurrentLimit(60)
+                    .withStatorCurrentLimitEnable(true)
+                    .withSupplyCurrentLimit(35)
+                    .withSupplyCurrentLimitEnable(true));
+
+    climberMotor.getConfigurator().apply(config);
+  }
+
+  
+  // Climber Up
+  public void climberUp() {
+    climberMotor.setControl(new DutyCycleOut(CLIMBER_UP_SPEED));
+  }
+
+   // Climber Down
+  public void climberDown() {
+    climberMotor.setControl(new DutyCycleOut(CLIMBER_DOWN_SPEED));
+  }
+
+  public void stop() {
+    climberMotor.setControl(new DutyCycleOut(0.0));
+  }
+
+}
