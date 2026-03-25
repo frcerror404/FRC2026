27e9b6009c828fd78f3553637b6780041952f3cb
Chad Krause <krauserobotics@chadkrause.com>
2026-03-24 20:56:16 -0400

    Add new autonomous routines and paths for 2025 season
    - Created new auto files for "LB Preload+Depot+HP", "LB Preload+Depot", "LB Preload+HP", "LB Preload", "LB To Depot", "MB Preload+Depot+HP", "MB Preload+Depot", "MB Preload", "RB Preload + Depot + HP", "RB Preload + HP".
    - Implemented sequential commands including aiming, shooting, feeding, and intake management.
    - Added paths for "HP to Preload", "LB Preload", "LB to Depot", "MB Preload", "Preload To Depot To Preload", "Preload To HP", and "RB Preload".
    - Updated settings.json with robot specifications and path configurations.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,14 +11,17 @@ import com.ctre.phoenix6.controls.SolidColor;
 import com.ctre.phoenix6.hardware.CANdle;
 // import com.ctre.phoenix6.hardware.CANdle;
 import com.ctre.phoenix6.signals.RGBWColor;
+import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.commands.AgitateIntake;
 import frc.robot.commands.DriveCommands;
 import frc.robot.commands.FeedFuel;
@@ -56,6 +59,7 @@ import frc.robot.subsystems.vision.VisionIO;
 import frc.robot.subsystems.vision.VisionIOLimelight;
 import frc.robot.util.CanDef;
 import frc.robot.util.CanDef.CanBus;
+import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -115,7 +119,7 @@ public class RobotContainer {
   // private final LimelightLoggerSubsystem limelightLoggerSubsystem;
 
   // Dashboard inputs
-  //   private final LoggedDashboardChooser<Command> autoChooser;
+  private final LoggedDashboardChooser<Command> autoChooser;
   private boolean slowMode = false;
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
@@ -203,24 +207,24 @@ public class RobotContainer {
 
     // Set up auto routines
 
-    // registerNamedCommands();
-    // autoChooser = new LoggedDashboardChooser<>("Auto Choices", AutoBuilder.buildAutoChooser());
+    registerNamedCommands();
+    autoChooser = new LoggedDashboardChooser<>("Auto Choices", AutoBuilder.buildAutoChooser());
 
     // Set up SysId routines
-    // autoChooser.addOption(
-    //     "Drive Wheel Radius Characterization", DriveCommands.wheelRadiusCharacterization(drive));
-    // autoChooser.addOption(
-    //     "Drive Simple FF Characterization", DriveCommands.feedforwardCharacterization(drive));
-    // autoChooser.addOption(
-    //     "Drive SysId (Quasistatic Forward)",
-    //     drive.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
-    // autoChooser.addOption(
-    //     "Drive SysId (Quasistatic Reverse)",
-    //     drive.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
-    // autoChooser.addOption(
-    //     "Drive SysId (Dynamic Forward)", drive.sysIdDynamic(SysIdRoutine.Direction.kForward));
-    // autoChooser.addOption(
-    //     "Drive SysId (Dynamic Reverse)", drive.sysIdDynamic(SysIdRoutine.Direction.kReverse));
+    autoChooser.addOption(
+        "Drive Wheel Radius Characterization", DriveCommands.wheelRadiusCharacterization(drive));
+    autoChooser.addOption(
+        "Drive Simple FF Characterization", DriveCommands.feedforwardCharacterization(drive));
+    autoChooser.addOption(
+        "Drive SysId (Quasistatic Forward)",
+        drive.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
+    autoChooser.addOption(
+        "Drive SysId (Quasistatic Reverse)",
+        drive.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
+    autoChooser.addOption(
+        "Drive SysId (Dynamic Forward)", drive.sysIdDynamic(SysIdRoutine.Direction.kForward));
+    autoChooser.addOption(
+        "Drive SysId (Dynamic Reverse)", drive.sysIdDynamic(SysIdRoutine.Direction.kReverse));
 
     // Configure the button bindings
     configureButtonBindings();
@@ -231,8 +235,6 @@ public class RobotContainer {
 
     // Register Commands for Auton
   }
-
-  private void trackCurrentDraw() {}
 
   /**
    * Use this method to define your button->command mappings. Buttons can be created by
@@ -333,7 +335,7 @@ public class RobotContainer {
    *
    * @return the command to run in autonomous //
    */
-  //   public Command getAutonomousCommand() {
-  //     // return autoChooser.get();
-  //   }
+  public Command getAutonomousCommand() {
+    return autoChooser.get();
+  }
 }
